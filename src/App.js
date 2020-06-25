29a38d98f8febd8e32b1f5eba746d179db945f6b
Federico Allegrini <federicoa@mondoconvenienza.net>
2020-06-25 12:11:08 +0200

    Hook - useState

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Post from "./Post/Post";
 
-class App extends Component {
-  state = {
+const App = (props) => {
+  const [postsState, setPostsState] = useState({
     posts: [
       { title: "Post 1", content: "This is Post 1" },
       { title: "Post 2", content: "This is Post 2" },
     ],
-    otherState: "Other",
-  };
+  });
 
-  updatePostHandler = () => {
-    this.setState({
+  const [otherState, setOtherState] = useState("Other");
+
+  const updatePostHandler = () => {
+    setPostsState({
       posts: [
         { title: "Post M", content: "Post Mod" },
         { title: "Post 2", content: "This is Post 2" },
@@ -20,24 +21,22 @@ class App extends Component {
     });
   };
 
-  render() {
-    return (
-      <div className="App">
-        <h1>React Posts!</h1>
-        <button onClick={this.updatePostHandler}>Change Post</button>
-        <Post
-          title={this.state.posts[0].title}
-          content={this.state.posts[0].content}
-        />
-        <Post
-          title={this.state.posts[1].title}
-          content={this.state.posts[1].content}
-        >
-          {new Date().toLocaleDateString()}
-        </Post>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <h1>React Posts!</h1>
+      <button onClick={updatePostHandler}>Change Post</button>
+      <Post
+        title={postsState.posts[0].title}
+        content={postsState.posts[0].content}
+      />
+      <Post
+        title={postsState.posts[1].title}
+        content={postsState.posts[1].content}
+      >
+        {new Date().toLocaleDateString()}
+      </Post>
+    </div>
+  );
+};
 
 export default App;
