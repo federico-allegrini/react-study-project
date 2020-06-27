4910fdd3337dc36a740e2d5eb312ad28020fa488
Federico Allegrini <federico.allegrini.dev@gmail.com>
2020-06-27 20:15:10 +0200

    Outputting lists, lists & state, state immutabily, keys, flexible lists

--- src/App.js
+++ src/App.js
@@ -5,34 +5,35 @@ import Post from "./Post/Post";
 class App extends Component {
   state = {
     posts: [
-      { title: "Post 1", content: "This is Post 1" },
-      { title: "Post 2", content: "This is Post 2" },
+      { id: "a1", title: "Post 1", content: "This is Post 1" },
+      { id: "a2", title: "Post 2", content: "This is Post 2" },
     ],
     otherState: "Other",
     postsVisibility: false,
   };
 
-  updatePostHandler = (newTitle) => {
-    this.setState({
-      posts: [
-        { title: newTitle, content: "Post Mod" },
-        { title: "Post 2", content: "This is Post 2" },
-      ],
+  titleChangedHandler = (event, id) => {
+    const postIndex = this.state.posts.findIndex((p) => {
+      return p.id === id;
     });
-  };
-
-  titleChangedHandler = (event) => {
-    this.setState({
-      posts: [
-        { title: "Post 1", content: "This is Post 1" },
-        { title: event.target.value, content: "This is Post 2" },
-      ],
-    });
+    // const post = Object.assign({}, this.state.posts[postIndex]);
+    const post = { ...this.state.posts[postIndex] };
+    post.title = event.target.value;
+    const posts = [...this.state.posts];
+    posts[postIndex] = post;
+    this.setState({ posts: posts });
   };
 
   togglePostHandler = () => {
     const isVisible = this.state.postsVisibility;
     this.setState({ postsVisibility: !isVisible });
+  };
+
+  deletePostHandler = (index) => {
+    // const posts = this.state.posts.slice();
+    const posts = [...this.state.posts];
+    posts.splice(index, 1);
+    this.setState({ posts: posts });
   };
 
   render() {
@@ -48,21 +49,19 @@ class App extends Component {
     if (this.state.postsVisibility) {
       posts = (
         <div>
-          <Post
-            title={this.state.posts[0].title}
-            content={this.state.posts[0].content}
-            click={this.updatePostHandler.bind(
-              this,
-              "Post Paragraph Modified... [BIND - Best way]"
-            )}
-          />
-          <Post
-            title={this.state.posts[1].title}
-            content={this.state.posts[1].content}
-            changed={this.titleChangedHandler}
-          >
-            {new Date().toLocaleDateString()}
-          </Post>
+          {this.state.posts.map((post, index) => {
+            return (
+              <Post
+                title={post.title}
+                content={post.content}
+                click={() => this.deletePostHandler(index)}
+                changed={(event) => this.titleChangedHandler(event, post.id)}
+                key={post.id}
+              >
+                {new Date().toLocaleDateString()}
+              </Post>
+            );
+          })}
         </div>
       );
     }
