04e0aea80c00c63bf1fdfa4840c6722bc6da1544
Federico Allegrini <federicoa@mondoconvenienza.net>
2020-07-02 12:46:12 +0200

    Component update lifecycle

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,6 +1,37 @@
 import React, { Component } from "react";
 import Post from "./Post/Post";
 class Posts extends Component {
+  // Not recommended with initial state undefined
+  // static getDerivedStateFromProps(props, state) {
+  //   console.log("[Posts.js] getDerivedStateFromProps");
+  //   return state;
+  // }
+
+  // Not recommended for use
+  // componentWillReceiveProps(props) {
+  //   console.log("[Posts.js] componentWillReceiveProps");
+  // }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("[Posts.js] shouldComponentUpdate");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("[Posts.js] getSnapshotBeforeUpdate");
+    return { message: "Snapshot!" };
+  }
+
+  // Not recommended for use
+  // componentWillUpdate() {
+  //   console.log("[Posts.js] componentWillUpdate");
+  // }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("[Posts.js] componentDidUpdate");
+    console.log(snapshot);
+  }
+
   render() {
     console.log("[Posts.js] rendering...");
     return this.props.posts.map((post, index) => {
