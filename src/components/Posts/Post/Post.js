ff27028da91510ba7032120449f20d912a61129d
Federico Allegrini <federicoa@mondoconvenienza.net>
2020-07-02 12:16:48 +0200

    Convert functional in class based components

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./Post.module.css";
-const post = (props) => {
-  console.log("[Post.js] rendering...");
-  return (
-    <div className={classes.Post}>
-      <h2 onClick={props.click}>{props.title}</h2>
-      <p>{props.content}</p>
-      <p>
-        <i>{props.children}</i>
-      </p>
-      <input onChange={props.changed} value={props.title}></input>
-    </div>
-  );
-};
-export default post;
+class Post extends Component {
+  render() {
+    console.log("[Post.js] rendering...");
+    return (
+      <div className={classes.Post}>
+        <h2 onClick={this.props.click}>{this.props.title}</h2>
+        <p>{this.props.content}</p>
+        <p>
+          <i>{this.props.children}</i>
+        </p>
+        <input onChange={this.props.changed} value={this.props.title}></input>
+      </div>
+    );
+  }
+}
+export default Post;
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 import Post from "./Post/Post";
-const posts = (props) => {
-  console.log("[Posts.js] rendering...");
-  return props.posts.map((post, index) => {
-    return (
-      <Post
-        title={post.title}
-        content={post.content}
-        click={() => props.clicked(index)}
-        changed={(event) => props.changed(event, post.id)}
-        key={post.id}
-      >
-        {new Date().toLocaleDateString()}
-      </Post>
-    );
-  });
-};
-export default posts;
+class Posts extends Component {
+  render() {
+    console.log("[Posts.js] rendering...");
+    return this.props.posts.map((post, index) => {
+      return (
+        <Post
+          title={post.title}
+          content={post.content}
+          click={() => this.props.clicked(index)}
+          changed={(event) => this.props.changed(event, post.id)}
+          key={post.id}
+        >
+          {new Date().toLocaleDateString()}
+        </Post>
+      );
+    });
+  }
+}
+export default Posts;
