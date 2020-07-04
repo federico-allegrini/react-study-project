384d2210d2d6abe17c0f5653258db1e4da4bcca6
Federico Allegrini <federico.allegrini.dev@gmail.com>
2020-07-04 13:09:22 +0200

    Rendering adjacent JSX elements - Fragment

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -1,18 +1,17 @@
-import React, { Component } from "react";
-import Auxiliary from "../../../hoc/Auxiliary";
+import React, { Component, Fragment } from "react";
 import classes from "./Post.module.css";
 class Post extends Component {
   render() {
     console.log("[Post.js] rendering...");
     return (
-      <Auxiliary>
+      <Fragment>
         <h2 onClick={this.props.click}>{this.props.title}</h2>
         <p>{this.props.content}</p>
         <p>
           <i>{this.props.children}</i>
         </p>
         <input onChange={this.props.changed} value={this.props.title}></input>
-      </Auxiliary>
+      </Fragment>
     );
   }
 }
