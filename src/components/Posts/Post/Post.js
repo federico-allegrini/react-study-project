91e13fc71777abb7fcf23990145e933160ece9c2
Federico Allegrini <federico.allegrini.dev@gmail.com>
2020-07-04 12:59:14 +0200

    Rendering adjacent JSX elements - Array

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -3,16 +3,20 @@ import classes from "./Post.module.css";
 class Post extends Component {
   render() {
     console.log("[Post.js] rendering...");
-    return (
-      <div className={classes.Post}>
-        <h2 onClick={this.props.click}>{this.props.title}</h2>
-        <p>{this.props.content}</p>
-        <p>
-          <i>{this.props.children}</i>
-        </p>
-        <input onChange={this.props.changed} value={this.props.title}></input>
-      </div>
-    );
+    return [
+      <h2 key="i1" onClick={this.props.click}>
+        {this.props.title}
+      </h2>,
+      <p key="i2">{this.props.content}</p>,
+      <p key="i3">
+        <i>{this.props.children}</i>
+      </p>,
+      <input
+        key="i4"
+        onChange={this.props.changed}
+        value={this.props.title}
+      ></input>,
+    ];
   }
 }
 export default Post;
